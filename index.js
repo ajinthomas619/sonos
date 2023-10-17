431e2983fa0ceecd2536cfcd755f5cf1e19b7fd9
ajin thomas <ajinthomas619@gmail.com>
2023-10-17 22:06:06 +0530

    sixteenth  commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const mongoose=require("mongoose");
 mongoose.connect("mongodb+srv://ajinthomas619:Motog31@cluster0.u9qv5iq.mongodb.net/",{
 useNewUrlParser: true,
 useUnifiedTopology:true
-}).then{
-  console.log("connected to mongo db");
-};
+}).then(() => {
+  console.log("Connected to MongoDB Atlas");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB Atlas: ", error);
+});
 const path = require('path')
 
 
